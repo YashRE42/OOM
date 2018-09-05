ee378d4eaba409629f6e49399943722b2b915687
Re42 <33805964+Re42@users.noreply.github.com>
2018-09-05 09:56:06 +0530

    create addSlot method to fill queue

--- assignment2/src/q1.java
+++ assignment2/src/q1.java
@@ -20,6 +20,10 @@ class course {
         this.priority=priority;
         this.numberOfSlots=numberOfSlots;
     }
+    
+    public void addSlot(int duration, String dayPrefs, String timePrefs) {
+        slots.add(new slot(duration, dayPrefs, timePrefs));
+    }
 }
 
 
