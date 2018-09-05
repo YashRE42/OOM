a91cb17dda5b1bed588b34e0ae397c344fee8724
Re42 <33805964+Re42@users.noreply.github.com>
2018-09-05 09:50:45 +0530

    create slot class

--- assignment2/src/q1.java
+++ assignment2/src/q1.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.util.*;
 
 public class q1 {
+}
+
+
+class slot{
+    private int duration;
+    private String dayPrefs; //preferences
+    private String timePrefs; //preferences
+    public slot(int duration, String dayPrefs, String timePrefs){
+        this.duration=duration;
+        this.dayPrefs=dayPrefs;
+        this.timePrefs=timePrefs;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
 }
