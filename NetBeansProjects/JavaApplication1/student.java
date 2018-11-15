a55e458079afdeaef8e60f667f4ac46b8a80ea32
Re42 <33805964+Re42@users.noreply.github.com>
2018-11-15 11:55:19 +0530

    upload files

---
+++ NetBeansProjects/JavaApplication1/student.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DELL
+ */
+public class student {
+
+private String roll;
+private String name;
+private String course; // (UG/PG/PhD, Dual Degree, Integrated MTech PhD)
+private String branch; // (IT/ECE)
+private String semester; // (1-8 for UG, 1-4 for PG, 1-10 for Dual Degree, no restrictions on others).
+
+}
